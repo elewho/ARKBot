137810b40ab223cbe21d8a2d57d671cd9d133919
elewho <daniellealota@yahoo.com>
2021-02-12 22:09:59 +1100

    finished verifying new joiners

--- src/main/java/ARKBot.java
+++ src/main/java/ARKBot.java
@@ -10,7 +10,7 @@ import javax.security.auth.login.LoginException;
 public class ARKBot {
 
     public static void main(String[] args) throws LoginException, InterruptedException {
-        JDA jda = JDABuilder.create("Nzc1NTYzMzExOTM1NzgyOTQy.X6oJng.5e-MEwDXFVjz4BWfy60QL7RRByA", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.GUILD_MEMBERS)
+        JDA jda = JDABuilder.create("", GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES, GatewayIntent.GUILD_MESSAGE_REACTIONS, GatewayIntent.GUILD_MEMBERS)
                 .addEventListeners(new MessageListener())
                 .addEventListeners(new ReactionListener())
                 .setActivity(Activity.playing("Counting dinos..."))
